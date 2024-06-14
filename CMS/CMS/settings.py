3935af283f478be82d39233d120b1779482b2b46
DejanPerovic <dejanp0@gmail.com>
2024-06-14 19:08:09 +0200

    Connected PostgresSQL with the app, created Patient entity, removed previous codes. #12

--- CMS/CMS/settings.py
+++ CMS/CMS/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "appointments",
+    "patient",
 ]
 
 MIDDLEWARE = [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "CMS.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "clinic_management_system",
+        "USER": "postgres",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
--- CMS/appointments/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:appointment_id>/', views.appointment_detail, name='appointment_detail'),
-]
